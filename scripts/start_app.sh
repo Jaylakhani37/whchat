23e2346ffc47a71966da516799153d88d3e92dfa
Jaylakhani37 <20it062@charusat.edu.in>
2024-01-19 02:52:58 +0530

    Fixed Deploy issues done

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Start the Streamlit application
-cd /home/ubuntu/whchat
-streamlit run app.py
+screen -dmS streamlit_app bash -c 'cd /home/ubuntu/whchat; streamlit run app.py --server.port 8501'
