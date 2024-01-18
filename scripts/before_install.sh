f04a7f51ed1da140416db474ca768ed59c6a8b80
Jaylakhani37 <20it062@charusat.edu.in>
2024-01-19 01:43:36 +0530

    Fixed script issues3

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Remove specific files that might cause conflicts
 rm -f /home/ubuntu/whchat/app.py
+rm -f /home/ubuntu/whchat/appspec.yml
+rm -f /home/ubuntu/whchat/buildspec.yml
+rm -f /home/ubuntu/whchat/scripts
