7549589f9b0ad04105be4354c60460e729484012
Jaylakhani37 <20it062@charusat.edu.in>
2024-01-19 02:07:44 +0530

    Fixed Deploy issues7

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Remove specific files that might cause conflicts
-rm -f /home/ubuntu/whchat/app.py
-rm -f /home/ubuntu/whchat/appspec.yml
-rm -f /home/ubuntu/whchat/buildspec.yml
-rm -f /home/ubuntu/whchat/helper.py
-rm -f /home/ubuntu/whchat/preprocessor.py
+rm -rf /home/ubuntu/whchat/
 rm -rf /home/ubuntu/whchat/scripts
