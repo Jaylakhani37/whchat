ddfbdc322a0b2f16e85c757a36141e870a68314b
Jaylakhani37 <20it062@charusat.edu.in>
2024-01-19 02:00:11 +0530

    Fixed Deploy issues5.

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,4 +3,6 @@
 rm -f /home/ubuntu/whchat/app.py
 rm -f /home/ubuntu/whchat/appspec.yml
 rm -f /home/ubuntu/whchat/buildspec.yml
+rm -f /home/ubuntu/whchat/helper.py
+rm -f /home/ubuntu/whchat/preprocessor.py
 rm -rf /home/ubuntu/whchat/scripts
